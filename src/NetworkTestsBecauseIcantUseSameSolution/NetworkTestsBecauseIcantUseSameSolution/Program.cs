4d310b549e303ff5a965888a4a2906a38b8e0ac2
Elijah H. Ellis <smurftown2@gmail.com>
2015-11-13 10:49:20 -0500

    Cleaned up directory and fixed bugs

--- src/NetworkTestsBecauseIcantUseSameSolution/NetworkTestsBecauseIcantUseSameSolution/Program.cs
+++ src/NetworkTestsBecauseIcantUseSameSolution/NetworkTestsBecauseIcantUseSameSolution/Program.cs
@@ -19,75 +19,82 @@ namespace NetworkTests
             NetworkBuilder Builder;
             
             Info = new NetworkInfo();
-            // Deprecated
-            Info.Class = AddressClass.B; // Deprecated 
-            Info.NumberOfHosts = 14;
-            Info.RequiredSubnets = 2;
-            Info.SampleAddress = "192.168.5.23";
-            Builder = new NetworkBuilder(Info);
-
-            SubnetDataWriter Datawriter = new SubnetDataWriter();
-            Datawriter.WriteSubnetData(Builder.BuiltNetwork);
-            Datawriter.WriteOutExcelDocument(Builder.BuiltNetwork, "14-2");
-
-            Info = new NetworkInfo();
-            // Deprecated
-            Info.Class = AddressClass.B; // Deprecated 
-            Info.NumberOfHosts = 14;
-            Info.RequiredSubnets = 16;
-            Info.SampleAddress = "192.168.5.23";
-            Builder = new NetworkBuilder(Info);
-
-            Datawriter.WriteOutExcelDocument(Builder.BuiltNetwork, "14-16");
-
-            Info = new NetworkInfo();
-            Info.NumberOfHosts = 126;
-            Info.RequiredSubnets = 2;
-            Info.SampleAddress = "192.168.5.23";
-            Builder = new NetworkBuilder(Info);
-
-            Datawriter.WriteOutExcelDocument(Builder.BuiltNetwork, "126-2");
-
-            Info = new NetworkInfo();
-            Info.NumberOfHosts = 62;
-            Info.RequiredSubnets = 4;
-            Info.SampleAddress = "192.168.5.23";
-            Builder = new NetworkBuilder(Info);
-
-            Datawriter.WriteOutExcelDocument(Builder.BuiltNetwork, "62-2");
-
-
-            Info = new NetworkInfo();
             Info.NumberOfHosts = 30;
             Info.RequiredSubnets = 8;
-            Info.SampleAddress = "192.168.5.23";
+            Info.SampleAddress = "172.16.2.5";
             Builder = new NetworkBuilder(Info);
 
-            Datawriter.WriteOutExcelDocument(Builder.BuiltNetwork, "30-8");
+            SubnetDataWriter Writer = new SubnetDataWriter();
+            Writer.WriteOutExcelDocument(Builder.BuiltNetwork, "waite");            
+            //// Deprecated
+            //Info.Class = AddressClass.B; // Deprecated 
+            //Info.NumberOfHosts = 14;
+            //Info.RequiredSubnets = 2;
+            //Info.SampleAddress = "192.168.5.23";
+            //Builder = new NetworkBuilder(Info);
 
-            Info = new NetworkInfo();
-            Info.NumberOfHosts = 6;
-            Info.RequiredSubnets = 32;
-            Info.SampleAddress = "192.168.5.23";
-            Builder = new NetworkBuilder(Info);
+            //SubnetDataWriter Datawriter = new SubnetDataWriter();
+            //Datawriter.WriteSubnetData(Builder.BuiltNetwork);
+            //Datawriter.WriteOutExcelDocument(Builder.BuiltNetwork, "14-2");
 
-            Datawriter.WriteOutExcelDocument(Builder.BuiltNetwork, "6-32");
+            //Info = new NetworkInfo();
+            //// Deprecated
+            //Info.Class = AddressClass.B; // Deprecated 
+            //Info.NumberOfHosts = 14;
+            //Info.RequiredSubnets = 16;
+            //Info.SampleAddress = "192.168.5.23";
+            //Builder = new NetworkBuilder(Info);
 
-            Info = new NetworkInfo();
-            Info.NumberOfHosts = 2;
-            Info.RequiredSubnets = 64;
-            Info.SampleAddress = "192.168.5.23";
-            Builder = new NetworkBuilder(Info);
+            //Datawriter.WriteOutExcelDocument(Builder.BuiltNetwork, "14-16");
 
-            Datawriter.WriteOutExcelDocument(Builder.BuiltNetwork, "2-64");
+            //Info = new NetworkInfo();
+            //Info.NumberOfHosts = 126;
+            //Info.RequiredSubnets = 2;
+            //Info.SampleAddress = "192.168.5.23";
+            //Builder = new NetworkBuilder(Info);
 
-            Info = new NetworkInfo();
-            Info.NumberOfHosts = 15;
-            Info.RequiredSubnets = 12;
-            Info.SampleAddress = "192.168.3.2";
-            Builder = new NetworkBuilder(Info);
+            //Datawriter.WriteOutExcelDocument(Builder.BuiltNetwork, "126-2");
 
-            Datawriter.WriteOutExcelDocument(Builder.BuiltNetwork, "Test");
+            //Info = new NetworkInfo();
+            //Info.NumberOfHosts = 62;
+            //Info.RequiredSubnets = 4;
+            //Info.SampleAddress = "192.168.5.23";
+            //Builder = new NetworkBuilder(Info);
+
+            //Datawriter.WriteOutExcelDocument(Builder.BuiltNetwork, "62-2");
+
+
+            //Info = new NetworkInfo();
+            //Info.NumberOfHosts = 30;
+            //Info.RequiredSubnets = 8;
+            //Info.SampleAddress = "192.168.5.23";
+            //Builder = new NetworkBuilder(Info);
+
+            //Datawriter.WriteOutExcelDocument(Builder.BuiltNetwork, "30-8");
+
+            //Info = new NetworkInfo();
+            //Info.NumberOfHosts = 6;
+            //Info.RequiredSubnets = 32;
+            //Info.SampleAddress = "192.168.5.23";
+            //Builder = new NetworkBuilder(Info);
+
+            //Datawriter.WriteOutExcelDocument(Builder.BuiltNetwork, "6-32");
+
+            //Info = new NetworkInfo();
+            //Info.NumberOfHosts = 2;
+            //Info.RequiredSubnets = 64;
+            //Info.SampleAddress = "192.168.5.23";
+            //Builder = new NetworkBuilder(Info);
+
+            //Datawriter.WriteOutExcelDocument(Builder.BuiltNetwork, "2-64");
+
+            //Info = new NetworkInfo();
+            //Info.NumberOfHosts = 15;
+            //Info.RequiredSubnets = 12;
+            //Info.SampleAddress = "192.168.3.2";
+            //Builder = new NetworkBuilder(Info);
+
+            //Datawriter.WriteOutExcelDocument(Builder.BuiltNetwork, "Test");
         }
 
 
